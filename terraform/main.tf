7a9d4f01bbe5ee5b80639f54dd052b74eda0c144
lyonhoe <lyonhoe@gmail.com>
2023-02-22 07:58:39 +0800

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -223,7 +223,7 @@ resource "aws_budgets_budget" "ec2" {
   name              = "budget-ec2-monthly"
   budget_type       = "COST"
   limit_amount      = "50"
-  limit_unit        = "SGD"
+  limit_unit        = "USD"
   time_period_end   = "2087-06-15_00:00"
   time_period_start = "2023-02-01_00:00"
   time_unit         = "MONTHLY"
