16e6fec3552fb15404cf40f94f958847a426b460
lyonhoe <lyonhoe@gmail.com>
2023-03-06 09:51:48 +0800

    removed aws conn arg from s3hook instance

--- dags/tasks/aws/postgres_to_s3.py
+++ dags/tasks/aws/postgres_to_s3.py
@@ -6,7 +6,7 @@ from airflow.hooks.S3_hook import S3Hook
 def _local_to_s3(
     bucket_name: str, key: str, file_name: str, remove_local: bool = False
 ):
-    s3 = S3Hook(aws_conn_id= 'aws_default')
+    s3 = S3Hook()
     s3.load_file(
         filename=file_name, bucket_name=bucket_name, replace=True, key=key
     )
